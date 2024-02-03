a655289b3bc2028a63c1e8be07bed1b209137114
wekir <skin_kirill@mail.ru>
2024-02-03 23:21:27 +0300

    add frontend and pasring JSON

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     MainWindow window;
-    window.setWindowTitle("Weather");
-    window.setWindowIcon(QIcon(":/images/icon.jpg"));
-    window.show();
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,12 +4,24 @@
 #include <QJsonObject>
 #include <QNetworkReply>
 #include <QUrl>
+#include <QTime>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowIcon (QIcon(":/images/icon.jpg"));
+    this->setWindowTitle("Weather");
+    this->setFixedSize (375,500);
+    ui->time->setText ("<html><head/><body><p align=\"center\"><span style=\" font-size:20pt; font-weight:700;\">" +
+                       QTime::currentTime().toString("hh:mm") + "</span></p></body></html>");
+
+    QBrush brush;
+    brush.setTextureImage (QImage(":/images/sky.jpg").scaled (375,500));
+    QPalette palete;
+    palete.setBrush (QPalette::Window, brush);
+    this->setPalette(palete);
 
     API_GEO = new QNetworkAccessManager(this);
     connect(API_GEO, SIGNAL(finished(QNetworkReply*)), this, SLOT(getGeo(QNetworkReply*)));
@@ -31,7 +43,11 @@ void MainWindow::getGeo(QNetworkReply *reply)
     API_WEATHER = new QNetworkAccessManager(this);
     connect(API_WEATHER, SIGNAL(finished(QNetworkReply*)), this, SLOT(getWeather(QNetworkReply*)));
     API_WEATHER->get(QNetworkRequest(QUrl(QString("https://api.weatherapi.com/v1/current.json?key=da0fd75db051489d85d121457240302&q=%1")
-                                              .arg (doc["city"].toString())))); //lang=ru
+                                              .arg (obj["city"].toString()))));
+
+    ui->city->setText ("<html><head/><body><p align=\"center\"><span style=\" font-size:23pt; font-weight:700;\">" +
+                       obj["city"].toString() + "</span></p></body></html>");
+
     reply->deleteLater ();
 }
 
@@ -39,5 +55,13 @@ void MainWindow::getWeather(QNetworkReply *reply)
 {
     QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
     QJsonObject obj = doc.object ();
+    QJsonValue val = obj.value("current");
+    obj = val.toObject ();
+    QString temp = QString::number(obj["temp_c"].toInt());
+
+    ui->temperature->setText ("<html><head/><body><p align=\"center\"><span style=\" font-size:34pt;\">" + temp
+                              + " C</span><span style=\" font-size:34pt; vertical-align:super;\">0</span></p></body></html>");
+
     reply->deleteLater ();
+    this->show ();
 }
